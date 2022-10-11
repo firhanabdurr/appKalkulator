da8f8e4df90f65d8b6d8dd548f76e63cae49ecf1
firhanabdurr <firhanabdurr@gmail.com>
2022-10-11 20:22:39 +0700

    tambah fungsi pembagian, dan fix angka desimal

--- script.js
+++ script.js
@@ -4,27 +4,32 @@ class Calculator {
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
+
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
+
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
+
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
+
     chooseOperation(operation) {
         if (this.currentOperand === '') return
         if (this.previousOperand !== '') {
-            this.compute() // meunang hasil operasi sebelumnya
+            this.compute()
         }
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
     }
+
     compute() {
         let computation
         const prev = parseFloat(this.previousOperand)
@@ -50,17 +55,42 @@ class Calculator {
         this.operation = undefined
         this.previousOperand = ''
     }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText =
+            this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
+
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const clearButton = document.querySelector('[data-clear]')
+const ClearButton = document.querySelector('[data-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -85,10 +115,11 @@ equalsButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
-clearButton.addEventListener('click', button => {
+ClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
 })
+
 deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
